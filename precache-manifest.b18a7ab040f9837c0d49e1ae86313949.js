521ed3d53e0a073fd4e6091c89668927ccf8f094
hilekat <68864060+hilekat@users.noreply.github.com>
2021-01-23 13:08:57 -0600

    Updates

---
+++ precache-manifest.b18a7ab040f9837c0d49e1ae86313949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c3e317f42d2f9885cc7ac355538af3",
+    "url": "/hmwrk19/index.html"
+  },
+  {
+    "revision": "c90aeebb651e6ab387c1",
+    "url": "/hmwrk19/static/css/main.0cb898ac.chunk.css"
+  },
+  {
+    "revision": "513965f2e78859965016",
+    "url": "/hmwrk19/static/js/2.dc043066.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/hmwrk19/static/js/2.dc043066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90aeebb651e6ab387c1",
+    "url": "/hmwrk19/static/js/main.02238ad4.chunk.js"
+  },
+  {
+    "revision": "358df65505d480c1ac42",
+    "url": "/hmwrk19/static/js/runtime-main.5b56086f.js"
+  }
+]);
